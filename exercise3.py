f4933d6b2e28c52ca3faff2047f7c8a1b5a77195
Code-Algo <alexanderacollins@gmail.com>
2022-07-11 16:20:18 -0500

    first commit

---
+++ exercise3.py
@@ -0,0 +1,4 @@
+### My failure = [a-zA-Z]+[,]+[\W]+[a-zA-Z]*|[\W][@][a-z]+
+
+## Was stuck trying to figure out what worked in reg expressions 101 :(
+# [\W][@][a-z] kinda worked for the twitter handle, so there's that.
